01a5b920bf313e8e22a544341ff515950c95ec05
mirko.serra <mirko.serra@flosslab.com>
2019-10-08 16:46:25 +0200

    Refactoring on crud table

--- crud.py
+++ crud.py
@@ -3,30 +3,37 @@ import configparser
 
 class Crud:
 
-    def init_config(self):
-        config = configparser.ConfigParser()
-        try:
-            return self.read_config_file(config)
-        except IOError:
-            self.write_config_file(self.create_config_file(config))
-            return self.read_config_file(config)
+    def __init__(self):
+        # https://stackoverflow.com/questions/576169/understanding-python-super-with-init-methods
+        super().__init__()
+        self.config_parser = configparser.ConfigParser()
 
-    def create_config_file(self, configparser):
-        configparser["PATH"] = {}
-        configparser["SAVE_PATH"] = {
+    def init_config(self):
+        try:
+            return self.read_config_file(self.config_parser)
+        except IOError:
+            self.write_config_file(self.create_config_file(self.config_parser))
+            return self.read_config_file(self.config_parser)
+
+    @staticmethod
+    def create_config_file(config_parser):
+        config_parser["PATH"] = {}
+        config_parser["SAVE_PATH"] = {
             'dir': ""
         }
-        configparser["TIMER"] = {
+        config_parser["TIMER"] = {
             'default': '1',
         }
 
-        return configparser
+        return config_parser
 
-    def read_config_file(self, configparser):
+    @staticmethod
+    def read_config_file(config_parser):
         with open('config.ini', 'r') as configfile:
-            configparser.readfp(configfile)
-            return configparser
+            config_parser.readfp(configfile)
+            return config_parser
 
-    def write_config_file(self, confiparser):
+    @staticmethod
+    def write_config_file(config_parser):
         with open('config.ini', 'w') as configfile:
-            confiparser.write(configfile)
+            config_parser.write(configfile)
